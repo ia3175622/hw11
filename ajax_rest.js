314990e8b2a1026228bb90ddc1493d84735cee15
ivanakulov <ia3175622@gmail.com>
2024-07-04 19:55:14 +0500

    chsnge ajax_rest

--- ajax_rest.js
+++ ajax_rest.js
@@ -1,13 +1,11 @@
 $(document).ready(function () {
   $("form").submit(function (event) {
     event.preventDefault();
-    var formData = {
-      query: $("#ip").val(),
-    };
-	var url = "http://suggestions.dadata.ru/suggestions/api/4_1/rs/iplocate/address?ip=";
-	var token = "3b262e0e81c99cffffbc00169a301ac7ab90c7fe";
-
-  fetch(url + query, {
+    let query = $("#ip").val();
+    var url = "http://suggestions.dadata.ru/suggestions/api/4_1/rs/iplocate/address?ip=";
+    var token = "3b262e0e81c99cffffbc00169a301ac7ab90c7fe";
+    
+    fetch(url + query, {
     method: "GET",
     mode: "cors",
     headers: {
@@ -16,9 +14,11 @@ $(document).ready(function () {
       "Authorization": "Token " + token
     }
   })
-
   .then(response => response.text())
-  .then(result => console.log(result))
+  .then(result => {
+    console.log(result);
+    document.getElementById("result").innerHTML = (result);
+  })
   .catch(error => console.log("error", error));
   });
 });
